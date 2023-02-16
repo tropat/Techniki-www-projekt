ae3e5838e9bdfdd4fd816b432fc865a8e10c1954
Pat <patrycjatr2001@gmail.com>
2023-02-17 00:41:10 +0100

    finish user ratings

--- filmoteka/routes/film.js
+++ filmoteka/routes/film.js
@@ -6,11 +6,37 @@ var router = express.Router();
 router.get('/:movieId', function(req, res, next) {
     if(req.cookies.userId) {
         var movie = null;
+        var rating = 0;
 
         var sql = 'SELECT * FROM ranking NATURAL JOIN filmy WHERE IdFilmu=' + req.params.movieId + ';';
         controller.db.query(sql, (error, data) => {
             movie = data;
-            res.render('film', {movie: movie[0], userLogin: req.cookies.userLogin});
+
+            sql = 'SELECT * FROM ocenyuzytkownika WHERE IdFilmu='+req.params.movieId+' AND IdUzytkownika='+req.cookies.userId+';';
+            controller.db.query(sql, (error, data) => {
+                if(data.length != 0) {
+                    rating = data[0].Ocena;
+                }
+                res.render('film', {movie: movie[0], userLogin: req.cookies.userLogin, userId: req.cookies.userId, rating:rating});
+            });
+
+        });
+    } else {
+        res.redirect("/logowanie");
+    }
+});
+
+router.post('/rate/:userId/:movieId/:number', function(req, res, next) {
+    if(req.cookies.userId) {
+        var sql = 'SELECT * FROM ocenyuzytkownika WHERE IdFilmu='+req.params.movieId+' AND IdUzytkownika='+req.params.userId+';';
+        controller.db.query(sql, (error, data) => {
+            if(data.length != 0) {
+                sql = 'UPDATE ocenyuzytkownika SET Ocena='+req.params.number+' WHERE IdFilmu='+req.params.movieId+' AND IdUzytkownika='+req.params.userId+';';
+                controller.db.query(sql, (error, data) => {});
+            } else {
+                sql = 'INSERT INTO ocenyuzytkownika VALUES(null, '+req.params.userId+','+req.params.movieId+','+req.params.number+');';
+                controller.db.query(sql, (error, data) => {});
+            }
         });
     } else {
         res.redirect("/logowanie");
--- filmoteka/routes/profil.js
+++ filmoteka/routes/profil.js
@@ -1,10 +1,24 @@
 var express = require('express');
+const controller = require("../controllers/controller");
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   if(req.cookies.userId) {
-    res.render('profil', {userLogin: req.cookies.userLogin});
+    var userData = null;
+    var userRatings = null;
+
+    var sql = 'SELECT * FROM uzytkownicy WHERE IdUzytkownika='+req.cookies.userId+';';
+    controller.db.query(sql, (error, data) => {
+      userData = data[0];
+
+      sql = 'SELECT * FROM ocenyuzytkownika NATURAL JOIN filmy WHERE IdUzytkownika='+req.cookies.userId+';';
+      controller.db.query(sql, (error, data) => {
+        userRatings = data;
+
+        res.render('profil', {userLogin: req.cookies.userLogin, userData: userData, userRatings: userRatings});
+      });
+    });
   } else {
     res.redirect("/logowanie");
   }
